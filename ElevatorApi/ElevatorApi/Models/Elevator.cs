aa8cbdc571c22cac65a82d4897477c9954e2fadc
David Boeser <davidmboeser@gmail.com>
2025-11-28 14:19:20 -0600

    Add classes to support elevator, floor, and floor request

--- ElevatorApi/ElevatorApi/Models/Elevator.cs
+++ ElevatorApi/ElevatorApi/Models/Elevator.cs
@@ -2,6 +2,24 @@
 {
     public class Elevator
     {
+        public Elevator()
+        {
+            this.Floors = new List<Floor>();
+            for (int i = 1; i <= 20; i++)
+            {
+                this.Floors.Add(new Floor(i));
+            }
+            this.Direction = Direction.Up;
+        }
 
+        public List<Floor> Floors { get; set; }
+        public List<FloorRequest> FloorRequests { get; set; }
+        public Direction Direction { get; set; }
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down
     }
 }
---
+++ ElevatorApi/ElevatorApi/Models/FloorRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ElevatorApi.Models
+{
+    public class FloorRequest
+    {
+        public int Floor { get; set; }
+        public FloorRequestType RequestType { get; set; }
+    }
+
+    public enum FloorRequestType
+    {
+        Passenger,
+        Call
+    }
+}
